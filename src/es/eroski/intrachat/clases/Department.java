44f280edce0126ed1cb867f918cabf3c535629a9
ikaslea <ikaslea@gela07_01>
2017-05-16 12:32:52 +0200

    Clases Department y Message implementado.

---
+++ src/es/eroski/intrachat/clases/Department.java
@@ -0,0 +1,38 @@
+package es.eroski.intrachat.clases;
+
+public class Department {
+
+	private int id_department;
+	private String name;
+	private int floor;
+
+	public Department() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId_department() {
+		return id_department;
+	}
+
+	public void setId_department(int id_department) {
+		this.id_department = id_department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getFloor() {
+		return floor;
+	}
+
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+}
---
+++ src/es/eroski/intrachat/clases/Message.java
@@ -0,0 +1,67 @@
+package es.eroski.intrachat.clases;
+
+import java.util.Date;
+
+public class Message {
+
+	private int id_message;
+	private int id_sender;
+	private int id_receiver;
+	private String subject;
+	private String text;
+	private Date date;
+
+	public Message() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId_message() {
+		return id_message;
+	}
+
+	public void setId_message(int id_message) {
+		this.id_message = id_message;
+	}
+
+	public int getId_sender() {
+		return id_sender;
+	}
+
+	public void setId_sender(int id_sender) {
+		this.id_sender = id_sender;
+	}
+
+	public int getId_receiver() {
+		return id_receiver;
+	}
+
+	public void setId_receiver(int id_receiver) {
+		this.id_receiver = id_receiver;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
