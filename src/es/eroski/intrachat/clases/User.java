6fc667d7595dfce7bd2367c220d1e339ea43d797
ikaslea <ikaslea@1aw3_07_11>
2017-05-16 12:29:38 +0200

    Conector y User terminado (falta poner el contenido de la array "messages")

--- src/es/eroski/intrachat/clases/User.java
+++ src/es/eroski/intrachat/clases/User.java
@@ -1,9 +1,75 @@
 package es.eroski.intrachat.clases;
 
+import java.util.ArrayList;
+
 public class User {
 
 	private int id_user;
 	private String name;
 	private String last_name;
-	
+	private String email;
+	private int id_department;
+	private ArrayList<User> contacts;
+	private ArrayList messages;
+
+	public User() {
+		super();
+	}
+
+	public int getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId_department() {
+		return id_department;
+	}
+
+	public void setId_department(int id_department) {
+		this.id_department = id_department;
+	}
+
+	public ArrayList<User> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(ArrayList<User> contacts) {
+		this.contacts = contacts;
+	}
+
+	public ArrayList getMessages() {
+		return messages;
+	}
+
+	public void setMessages(ArrayList messages) {
+		this.messages = messages;
+	}
+
 }
