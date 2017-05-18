0acb5a872c3e0d4892dd692c5f4c1fc07c2a4729
Leire <Leire@10.22.72.186>
2017-05-18 09:54:15 +0200

    selectUsersFromDepartment();

--- src/es/eroski/intrachat/models/UserModel.java
+++ src/es/eroski/intrachat/models/UserModel.java
@@ -33,13 +33,39 @@ public class UserModel extends Conector {
 		}
 		return null;
 	}
+	
+	public ArrayList<User> selectUsersFromDepartment(String department) {
+		ArrayList<User> users = new ArrayList<User>();
+		try {
+			Statement st = this.conexion.createStatement();
+			ResultSet rs = st.executeQuery("select * from users where department='" + department + "'");
+			while (rs.next()) {
+				User user = new User();
 
+				user.setId_user(rs.getInt("id_user"));
+				user.setPassword(rs.getString("password"));
+				user.setName(rs.getString("name"));
+				user.setLast_name(rs.getString("last_name"));
+				user.setEmail(rs.getString("email"));
+				user.setId_department(rs.getInt("id_department"));
+				users.add(user);
+			}
+			return users;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	public User selectUserLogin(String userlogin, String password) {
+		int id_user = Integer.parseInt(userlogin);
+		String email = userlogin;
 		User user = null;
 		try {
 			Statement st = this.conexion.createStatement();
-			ResultSet rs = st.executeQuery("select * from users where id_user='" + Integer.parseInt(userlogin) + 
-										   "' or email='" + userlogin + "' and password='" + password + "'");
+			ResultSet rs = st.executeQuery("select * from users where id_user='" + id_user + 
+										   "' or email='" + email + "' and password='" + password + "'");
 			rs.next();
 			user = new User();
 			user.setId_user(rs.getInt("id_user"));
@@ -56,6 +82,8 @@ public class UserModel extends Conector {
 		}
 		return user;
 	}
+	
+	
 	
 	public void insertUser(User user) {
 		Statement st;
